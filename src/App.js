54945d6abee76f356821cca41b8773d6532f6d15
MichalCohen058 <110355948+MichalCohen058@users.noreply.github.com>
2022-08-25 22:49:39 +0300

    add code in userList component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Register from './Components/Register/Register';
 import UserDetails from './Components/UserDetails/UserDetails';
 import FillQuestionnaire from './Components/FillQuestionnaire/FillQuestionnaire';
+import UserList from './Components/UserList/UserList';
 
 function App() {
   return (<>
   <Register/>
   <UserDetails/>
   <FillQuestionnaire/>
+  <UserList/>
   </>
     
   );
--- src/Components/UserList/UserList.js
+++ src/Components/UserList/UserList.js
@@ -1,7 +1,50 @@
 import "../UserList/UserList.css"
 export default function UserList(){
-
+const userArr=[
+    {
+    firstName:"yosef",
+    lastName:"cohen",
+    age:22,
+    status:"ravak",
+    city:"beytar",
+    height:1.75,
+    healthCondition:"good",
+    look:"pretty"
+    },
+    {
+        firstName:"inon",
+        lastName:"cohen",
+        age:22,
+        status:"ravak",
+        city:"givat shaul",
+        height:1.73,
+        healthCondition:"good",
+        look:"pretty and nice"
+    },
+    {
+        firstName:"aharon",
+        lastName:"avargel",
+        age:23,
+        status:"ravak",
+        city:"bney-brak",
+        height:1.80,
+        healthCondition:"good",
+        look:"sweet so beautiful!!!"
+    }
+]
 
     return(<>
+    <h1>UserList!!!</h1>
+    <input type="text"/>
+    <input type="text"/>
+    <input type="text"/>
+    <input type="text"/>
+    <input type="text"/>
+    <input type="text"/>
+    <input type="text"/>
+    <input type="text"/>
+
+    {userArr.map(u=><p key={u}>{u.firstName} | {u.lastName} | {u.age} | {u.status} | {u.city} | {u.height}
+    | {u.healthCondition} | {u.look}</p>)}
     </>)
 }
